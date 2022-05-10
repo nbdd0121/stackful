6773d00226dce83b286071fba1597ec3bf8d71d6
Gary Guo <gary@garyguo.net>
2022-05-10 15:38:33 +0100

    Better stack trace for x86_64

--- src/arch/x86_64.s
+++ src/arch/x86_64.s
@@ -35,25 +35,78 @@ fiber_restore_ret_raw:
 .type fiber_enter, @function
 fiber_enter:
 _fiber_enter:
+.cfi_startproc
+    # Top of the fresh stack, we use these to store the last function that
+    # calls fiber_enter/fiber_switch_enter so that the stack trace can continue
+    # past this function.
+    sub rdi, 16
+    mov rax, [rsp]
+    mov [rdi + 8], rax
+    mov rax, rsp
+    add rax, 8
+    mov [rdi], rax
+
     call fiber_save_raw
-    # Switch stack and enter
+
+    # Switch stack
     xchg rsp, rdi
+
+    # CFI metadata to instruct unwinder to find our saved info at the top of stack.
+    .cfi_def_cfa rsp, 16
+    .cfi_offset rsp, -16
+    .cfi_offset rip, -8
+
+    # Save the top-of-stack address in old stack frame; otherwise this will be lost after a switch
+    mov [rdi - 8], rsp
+
     call rdx
     ud2
 .size fiber_enter, .-fiber_enter
+.cfi_endproc
 
-# fiber_switch: fn(StackPointer, usize) -> SwitchResult
+# fiber_switch_enter: fn(StackPointer, usize) -> SwitchResult
 .global fiber_switch_enter
 .global _fiber_switch_enter
-.global fiber_switch_leave
-.global _fiber_switch_leave
 fiber_switch_enter:
 _fiber_switch_enter:
-fiber_switch_leave:
-_fiber_switch_leave:
+    # Extract the saved top-of-stack address
+    mov rcx, [rdi - 8]
+
+    # Fill the address with new caller info for a proper stack trace
+    mov rax, [rsp]
+    mov [rcx + 8], rax
+    mov rax, rsp
+    add rax, 8
+    mov [rcx], rax
+
     call fiber_save_raw
+
     # Switch stack
     mov rax, rsp
     mov rsp, rdi
     mov rdx, rsi
+
+    # Save the top-of-stack address in old stack frame again.
+    mov [rax - 8], rcx
+
+    jmp fiber_restore_ret_raw
+
+# fiber_switch_leave: fn(StackPointer, usize) -> SwitchResult
+.global fiber_switch_leave
+.global _fiber_switch_leave
+fiber_switch_leave:
+_fiber_switch_leave:
+    call fiber_save_raw
+
+    # Extract the saved top-of-stack address
+    mov rcx, [rdi - 8]
+
+    # Switch stack
+    mov rax, rsp
+    mov rsp, rdi
+    mov rdx, rsi
+
+    # Save the top-of-stack address
+    mov [rax - 8], rcx
+
     jmp fiber_restore_ret_raw
